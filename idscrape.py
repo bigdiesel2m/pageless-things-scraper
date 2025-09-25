0c236bf6459d141819f62f1617684738f6441785
Daniel <bigdiesel2m@yahoo.com>
2025-09-25 12:10:40 -0400

    convert from using smw to using bucket

--- idscrape.py
+++ idscrape.py
@@ -5,51 +5,43 @@ session = requests.Session()
 headers = {'User-Agent': 'Automated scraping script to generate list of page IDs on the wiki. See https://github.com/bigdiesel2m/pageless-things-scraper'}
 
 obj_ids = []
-for i in range(13): #up to 65000 objects
-	offset = 0
-	while True:
-		print(str(i) + " - " + str(offset))
-		bound = i*5000
-		params = {
-			"action": "ask",
-			"format": "json",
-			"query": '[[Object ID::>' + str(bound) + ']][[Object ID::<<' + str(bound+5000) + ']]|?Object ID|limit=500|offset=' + str(offset)
-		}
-		response_json = session.get(API_URL, params=params, headers=headers).json()
-		for result in response_json['query']['results']:
-			name = result
-			for obj_id in response_json['query']['results'][result]['printouts']['Object ID']:
-				obj_ids.append(str(obj_id))
-		
-		if 'query-continue-offset' in response_json:
-			offset = response_json['query-continue-offset']
-		else:
-			break
+offset = 0
+while True:
+	print(str(offset))
+	params = {
+		"action": "bucket",
+		"format": "json",
+		"query": "bucket('object_id').select('id').limit(5000).offset(" + str(offset) + ").run()"
+	}
+	response_json = session.get(API_URL, params=params, headers=headers).json()
+	for result in response_json['bucket']:
+		for obj_id in result['id']:
+			obj_ids.append(str(obj_id))
+	if len(response_json['bucket']) == 5000:
+		offset = offset + 5000
+	else:
+		break
 
 with open('obj_ids.txt', "w", encoding="utf-8") as outfile:
 	outfile.write('\n'.join(obj_ids))
 
 npc_ids = []
-for i in range(4): #up to 20000 npcs
-	offset = 0
-	while True:
-		print(str(i) + " - " + str(offset))
-		bound = i*5000
-		params = {
-			"action": "ask",
-			"format": "json",
-			"query": '[[NPC ID::>' + str(bound) + ']][[NPC ID::<<' + str(bound+5000) + ']]|?NPC ID|limit=500|offset=' + str(offset)
-		}
-		response_json = session.get(API_URL, params=params, headers=headers).json()
-		for result in response_json['query']['results']:
-			name = result
-			for npc_id in response_json['query']['results'][result]['printouts']['NPC ID']:
-				npc_ids.append(str(npc_id))
-
-		if 'query-continue-offset' in response_json:
-			offset = response_json['query-continue-offset']
-		else:
-			break
+offset = 0
+while True:
+	print(str(offset))
+	params = {
+		"action": "bucket",
+		"format": "json",
+		"query": "bucket('npc_id').select('id').limit(5000).offset(" + str(offset) + ").run()"
+	}
+	response_json = session.get(API_URL, params=params, headers=headers).json()
+	for result in response_json['bucket']:
+		for npc_id in result['id']:
+			npc_ids.append(str(npc_id))
+	if len(response_json['bucket']) == 5000:
+		offset = offset + 5000
+	else:
+		break
 
 with open('npc_ids.txt', "w", encoding="utf-8") as outfile:
 	outfile.write('\n'.join(npc_ids))
